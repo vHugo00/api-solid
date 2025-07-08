babced6be304206c690884eaa23425ba0e5ab8d5
Vitor Hugo <vv001226@gmail.com>
2025-07-08 16:42:43 -0300

    Fundamentos do Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from 'generated/prisma'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Vitor',
+    email: 'vv001226@gmail.com',
+  },
+})
