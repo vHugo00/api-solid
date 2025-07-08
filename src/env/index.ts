df1b0c209cabdfee7b059b103a71a6fc72cf1521
Vitor Hugo <vv001226@gmail.com>
2025-07-07 22:40:16 -0300

    Configurando ESLint

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,17 +1,17 @@
-import "dotenv/config";
-import { z } from "zod";
+import 'dotenv/config'
+import { z } from 'zod'
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
-});
+})
 
-const _env = envSchema.safeParse(process.env);
+const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
-  console.error("⚠️ Invalid enviroment variales ⚠️", _env.error.format);
+  console.error('⚠️ Invalid enviroment variales ⚠️', _env.error.format)
 
-  throw new Error("Invalid enviroment variales.");
+  throw new Error('Invalid enviroment variales.')
 }
 
-export const env = _env.data;
+export const env = _env.data
