a953a039b6f7c3dc0c56bace38633110ac403192
Vitor Hugo <vv001226@gmail.com>
2025-07-11 19:22:56 -0300

    Repository Pattern

---
+++ src/repositories/prisma-users-repository.ts
@@ -0,0 +1,11 @@
+import { prisma } from '@/lib/prisma'
+import { Prisma } from 'generated/prisma'
+
+export class PrismaUsersRepository {
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data,
+    })
+    return user
+  }
+}
